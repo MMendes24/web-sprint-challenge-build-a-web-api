81168180674d7847f2efe1557cb1e970533f793a
MMendes24 <mmbusiness1@protonmail.com>
2020-09-04 11:00:28 -0400

    MVP achieved.

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -13,4 +13,55 @@ router.get("/", (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+    .then(thenRes => {
+        res.status(200).json(thenRes)
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Internal server error."})
+    })
+})
+
+router.get('/:id/actions', (req, res) => {
+    Projects.get(req.params.id)
+    .then(thenRes => {
+        res.status(200).json(thenRes.actions)
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Internal server error."})
+    })
+})
+
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+    .then(thenRes => {
+        res.status(201).json(thenRes)
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Action could not be created."})
+    })
+})
+
+router.put('/:id', (req, res) => {
+    Projects.update(req.params.id, req.body)
+    .then(thenRes => {
+        res.status(200).json(thenRes)
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Action could not be edited."})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Projects.remove(req.params.id)
+    .then(thenRes => {
+        res.status(200).send("Action successfully deleted.")
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Action could not be deleted."})
+    })
+})
+
+
 module.exports = router
